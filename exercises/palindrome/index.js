97bd5f61092b4377a1a8c747614ce3f602d7ff8f
김동욱 <igerapex@gmail.com>
2020-07-30 06:10:22 +0900

    Add solutions for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -16,4 +16,17 @@ function palindrome(str) {
   return true;
 }
 
+// Solution #1
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
+
+// Solution #2
+function palindrome(str) {
+  return str.split('').every((char, i) => {
+    return char === str[str.length - i - 1]
+  });
+}
+
 module.exports = palindrome;

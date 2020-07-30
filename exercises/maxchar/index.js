63769ed443a538741e07928daf0734102124defc
dwook <igerapex@gmail.com>
2020-07-30 17:09:59 +0900

    [Done] maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,17 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const count = {};
+  for (let character of str) {
+    if (count[character]) {
+      count[character]++;
+    } else {
+      count[character] = 1;
+    }
+  }
+  const sorted = Object.entries(count).sort((a, b) => b[1] - a[1])
+  return sorted[0][0]
+}
 
 module.exports = maxChar;

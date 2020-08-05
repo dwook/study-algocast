4512cfab58e090dd840f550d5b5632e197756f1e
김동욱 <igerapex@gmail.com>
2020-08-06 00:05:20 +0900

    Add solutions for chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,12 +9,41 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-
-  const arr = []
+  const arr = [];
   while (array.length) {
-    arr.push(array.splice(0, size))
+    arr.push(array.splice(0, size));
   }
   return arr;
+}
+
+// Solution #1
+function chunk(array, size) {
+  const chunked = [];
+
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+
+  return chunked;
+}
+
+// Solution #2
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
 }
 
 module.exports = chunk;

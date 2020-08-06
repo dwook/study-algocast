d769fee867d6379d856aaad1b436387a2c29e2fd
김동욱 <igerapex@gmail.com>
2020-08-06 23:12:03 +0900

    Add solutions for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -19,7 +19,46 @@ function anagrams(stringA, stringB) {
       strB.splice(index, 1);
     }
   }
-  return strA.length === 0 && strB.length === 0  ? true : false;
+
+  return strA.length === 0 && strB.length === 0 ? true : false;
+}
+
+// Solution #1
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA);
+  const bCharMap = buildCharMap(stringB);
+
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for (let char in aCharMap) {
+    if (aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
+
+
+// Solution #2
+function anagrams(stringA, stringB) {
+  return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+  return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
